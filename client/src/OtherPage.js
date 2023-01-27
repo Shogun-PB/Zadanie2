c1220282368d57c27936c0d3b74f5abf9163bf24
shogun-pb <patrykbak1999@gmail.com>
2023-01-27 19:25:27 +0100

    init comit

---
+++ client/src/OtherPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      Multifib
+      Patryk Bąk
+      I2S 2.1
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
